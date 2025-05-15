542b6fb5fb703ac9575641b3b0732108fd9448f6
tmp <you@example.com>
2025-05-15 15:36:21 +0300

    Model interface added

--- Graphics-EmptyProject/Model.h
+++ Graphics-EmptyProject/Model.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include"../mp2-lab6-tables/SortTable.h"
+#include"../mp2-lab6-tables/ScanTable.h"
+#include"../mp2-lab6-tables/ListHashTable.h"
+#include"../mp2-lab6-tables/BalanceTreeTable.h"
+#include"../mp2-lab6-tables/ArrayHashTable.h"
+#include"../mp2-lab6-tables/TreeTable.h"
+
+#include<vector>
+
+class Model {
+  Table<int, double>* maintable;
+  std::vector<Record<int, double>> recordsVec;
+
+public:
+  Model();
+
+  void initSortTable();
+  void initScanTable();
+  void initListHashTable();
+  void initArrayHashTable();
+  void initTreeTable();
+  void initAVLTreeTable();
+
+  void generateRecords(int amount, int maxKey);
+  void deleteAllrecords();
+
+  bool findRecord(int key);
+  void deleteRecord(int key);
+  void insertRecord(Record<int, double> rec);
+
+  int getEfficiency();
+};
