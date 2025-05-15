a243a0b250920c89596e4229cc89ce3ccf9a1c29
tmp <you@example.com>
2025-05-15 15:47:20 +0300

    Model constructor and init methods added

--- Graphics-EmptyProject/Model.cpp
+++ Graphics-EmptyProject/Model.cpp
@@ -1,1 +1,33 @@
-asd
+#include"Model.h"
+
+Model::Model() {
+  maintable = nullptr;
+}
+
+void Model::initSortTable() {
+  maintable = new SortTable<int, double>();
+}
+void Model::initScanTable() {
+  maintable = new ScanTable<int, double>();
+}
+void Model::initListHashTable() {
+  maintable = new ListHashTable<int, double>();
+}
+void Model::initArrayHashTable() {
+  maintable = new ArrayHashTable<int, double>();
+}
+void Model::initTreeTable() {
+  maintable = new TreeTable<int, double>();
+}
+void Model::initAVLTreeTable() {
+  maintable = new AVLTreeTable<int, double>();
+}
+
+void Model::generateRecords(int amount, int maxKey);
+void Model::deleteAllrecords();
+
+bool Model::findRecord(int key);
+void Model::deleteRecord(int key);
+void Model::insertRecord(Record<int, double> rec);
+
+int Model::getEfficiency();
